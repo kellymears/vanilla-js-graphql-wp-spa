8be5bdfe16720cb5f5a7d772846971e87b502d56
Kelly Mears <developers@tinypixel.dev>
2019-05-23 23:50:57 -0700

    :recycle: Clean up example src

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,35 +1,17 @@
 const mix = require ('laravel-mix')
 mix.pug = require('laravel-mix-pug')
 
-mix.setResourceRoot(__dirname + '/src')
+mix.setResourceRoot(`${__dirname}/src`)
 
-mix.browserSync(({
-  proxy: 'http://ndnstatic.valet',
-  files: [
-    `./src/**/*.js`,
-    `./src/**/*.css`,
-  ],
-}))
-
-mix.postCss('./src/css/app.css', 'dist/css', [
+mix.postCss('src/css/app.css', 'dist/css', [
   require('postcss-import'),
   require('tailwindcss'),
-  require('postcss-preset-env'),
-  require('postcss-hexrgba'),
-  require('postcss-nested'),
-  require('postcss-url'),
-  require('cssnano')({
-    preset: 'default'
-  }),
 ])
+
+mix.js('src/app.js', 'dist/js')
 
 mix.pug('./src/pug/*.pug', '../../dist')
 
-.js('src/app.js', 'dist/js')
-.copyDirectory('src/assets/images','dist/images')
-.copyDirectory('src/assets/svg','dist/svg')
-.copyDirectory('src/assets/fonts','dist/fonts')
-
-
-.setPublicPath('dist')
-.options({ processCssUrls: false })
+mix.setPublicPath('dist').options({
+  processCssUrls: false
+})
